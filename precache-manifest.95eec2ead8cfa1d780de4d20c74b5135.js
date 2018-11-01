6b3da9c3ef4201c9c2d8ee68973d7ded6a6b3986
sibhat <sibhat.temelso@gmail.com>
2018-11-01 10:52:23 -0700

    Updates

---
+++ precache-manifest.95eec2ead8cfa1d780de4d20c74b5135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f72454a2293557bf071a5ec812ac7cb",
+    "url": "/sibhat/Conways-Life.git/static/media/jack-o-lantern-silhouette-12.0f72454a.png"
+  },
+  {
+    "revision": "3e0be52cdf6dacdbc6e9",
+    "url": "/sibhat/Conways-Life.git/static/js/runtime~main.3e0be52c.js"
+  },
+  {
+    "revision": "ac4d7610cbe58e54362d",
+    "url": "/sibhat/Conways-Life.git/static/js/main.ac4d7610.chunk.js"
+  },
+  {
+    "revision": "04ee7cd979d260d46c11",
+    "url": "/sibhat/Conways-Life.git/static/js/1.04ee7cd9.chunk.js"
+  },
+  {
+    "revision": "ac4d7610cbe58e54362d",
+    "url": "/sibhat/Conways-Life.git/static/css/main.c31cfa50.chunk.css"
+  },
+  {
+    "revision": "202a35bb1b8456cf2558157b3e1e93a7",
+    "url": "/sibhat/Conways-Life.git/index.html"
+  }
+];
